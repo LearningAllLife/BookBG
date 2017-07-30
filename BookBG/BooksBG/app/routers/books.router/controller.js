da4d8fdb54f6c17b1d0086b8df9f815d3b2b9e1e
Vasil Kamburov <vasil.kamburov@abv.bg>
2017-07-30 16:24:22 +0300

    c

--- BookBG/BooksBG/app/routers/books.router/controller.js
+++ BookBG/BooksBG/app/routers/books.router/controller.js
@@ -211,76 +211,77 @@ class BooksConroller {
         return this.data.books.getById(bookId)
             .then((book) => {
                 return Promise.all(
-                        [
-                            this.data.genres.getAll({ _name: book._genre }),
-                            this.data.authors.getAll({ _name: book._author }),
-                            this.data.books.update({ _title: book._title }, { $set: { '_isDeleted': true } }),
-                        ])
-                    .then((result) => {
-                        let genreBooks = result[0][0]._books;
-                        let authorBooks = result[1][0]._books;
-                        const b = book;
-
-                        for (let i = 0; i < genreBooks.length; i++) {
-                            const currentBook = genreBooks[i];
-
-                            if (currentBook._title === b._title &&
-                                currentBook._author === b._author) {
-                                genreBooks = genreBooks.splice(i, 1);
-                                break;
-                            }
-                        }
-
-
-                        for (let i = 0; i < authorBooks.length; i++) {
-                            const currentBook = authorBooks[i];
-
-                            if (currentBook._title === b._title &&
-                                currentBook._author === b._author) {
-
-                                authorBooks = authorBooks.splice(i, 1);
-                                break;
-                            }
-                        }
-
-                        const genre = result[0][0];
-                        const author = result[1][0];
-
-                        this.data.genres.update({ _name: book._genre }, genre);
-                        this.data.authors.update({ _name: book._author }, author);
-                    });
-            });
-    }
-
-    getAllByFilter(req, res, filter) {
-        let user = req.user;
-        const route = req.path.slice(1, req.path.indexOf('/', 1));
-        const page = req.params.page || 1;
-        const skip = (page - 1) * PAGESIZE;
-        const limit = PAGESIZE;
-        let totalCount = 0;
-
-        return this.data.books.count(filter)
-            .then((count) => {
-                totalCount = count;
-                return this.data.books.getAll(filter, {}, skip, limit);
+                    [
+                        this.data.genres.getAll({ _name: book._genre }),
+                        this.data.authors.getAll({ _name: book._author }),
+                        this.data.books.update({ _title: book._title }, { $set: { '_isDeleted': true } }),
+                    ])
             })
-            .then((books) => {
-                const pagesNumber = Math.ceil(totalCount / PAGESIZE);
-                const indeces = [];
+            .then((result) => {
+                let genreBooks = result[0][0]._books;
+                let authorBooks = result[1][0]._books;
+                const b = book;
 
-                for (let a = 1; a <= pagesNumber; a += 1) {
-                    indeces.push(a);
+                for (let i = 0; i < genreBooks.length; i++) {
+                    const currentBook = genreBooks[i];
+
+                    if (currentBook._title === b._title &&
+                        currentBook._author === b._author) {
+                        genreBooks = genreBooks.splice(i, 1);
+                        break;
+                    }
                 }
 
-                if (!user) {
-                    user = {};
-                    user._isAdmin = false;
+
+                for (let i = 0; i < authorBooks.length; i++) {
+                    const currentBook = authorBooks[i];
+
+                    if (currentBook._title === b._title &&
+                        currentBook._author === b._author) {
+
+                        authorBooks = authorBooks.splice(i, 1);
+                        break;
+                    }
                 }
 
-                res.render('books/partialViews/booksContent.pug', { context: books, isAdmin: user._isAdmin, indeces: indeces, marker: route });
+                const genre = result[0][0];
+                const author = result[1][0];
+
+                this.data.genres.update({ _name: book._genre }, genre);
+                this.data.authors.update({ _name: book._author }, author);
             });
-    }
+    });
+}
+
+getAllByFilter(req, res, filter) {
+    let user = req.user;
+    const route = req.path.slice(1, req.path.indexOf('/', 1));
+    const page = req.params.page || 1;
+    const skip = (page - 1) * PAGESIZE;
+    const limit = PAGESIZE;
+    let totalCount = 0;
+
+    return this.data.books.count(filter)
+        .then((count) => {
+            totalCount = count;
+            return this.data.books.getAll(filter, {}, skip, limit);
+        })
+        .then((books) => {
+            const pagesNumber = Math.ceil(totalCount / PAGESIZE);
+            const indeces = [];
+
+            for (let a = 1; a <= pagesNumber; a += 1) {
+                indeces.push(a);
+            }
+
+            if (!user) {
+                user = {};
+                user._isAdmin = false;
+            }
+
+            res.render('books/partialViews/booksContent.pug', { context: books, isAdmin: user._isAdmin, indeces: indeces, marker: route });
+        });
+}
 }
 
 const init = (data) => {
